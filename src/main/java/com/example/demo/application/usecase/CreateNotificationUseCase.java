53128223a59002c50e4eac3f4ebcf81a45254ad9
ffn <fanuelson@gmail.com>
2026-01-22 00:00:52 -0300

    refact: CreateNotificationUseCase

--- src/main/java/com/example/demo/application/usecase/CreateNotificationUseCase.java
+++ src/main/java/com/example/demo/application/usecase/CreateNotificationUseCase.java
@@ -1,9 +1,9 @@
 package com.example.demo.application.usecase;
 
 import com.example.demo.application.port.in.CreateNotificationCommand;
-import com.example.demo.domain.model.Notification;
 import com.example.demo.application.port.out.event.NotificationEvent;
 import com.example.demo.application.port.out.event.NotificationEventPublisher;
+import com.example.demo.domain.model.Notification;
 import com.example.demo.domain.repository.NotificationRepository;
 import com.example.demo.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,15 +18,20 @@ public class CreateNotificationUseCase {
   private final NotificationEventPublisher notificationEventPublisher;
 
   public void execute(final CreateNotificationCommand command) {
-    final var userId = command.userId();
-    final var transactionId = command.transactionId();
-    final var msg = command.msg();
-
-    final var user = userRepository.findById(userId).orElseThrow();
+    final var user = userRepository
+      .findById(command.userId())
+      .orElseThrow();
 
     user.getEnabledNotificationChannels()
       .stream()
-      .map(Notification.fromChannel(transactionId, userId, user.getEmail(), msg))
+      .map(
+        Notification.fromChannel(
+          command.transactionId(),
+          command.userId(),
+          user.getEmail(),
+          command.msg()
+        )
+      )
       .map(notificationRepository::save)
       .map(NotificationEvent.fromNotification())
       .forEach(notificationEventPublisher::publish);
